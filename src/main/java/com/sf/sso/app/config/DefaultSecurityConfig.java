c7e380be4f4eb32f7a330d6c4ec454649f5a11a4
salman <salmandriva@gmail.com>
2024-12-04 11:55:20 +0700

    memindahkan settingan oauth registration dari application.yml ke java

--- src/main/java/com/sf/sso/app/config/DefaultSecurityConfig.java
+++ src/main/java/com/sf/sso/app/config/DefaultSecurityConfig.java
@@ -91,58 +91,58 @@ public class DefaultSecurityConfig {
         return http.build();
     }
     
-//    @Bean 
-//	public RegisteredClientRepository registeredClientRepository() {
-//		RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
-//				.clientId("oidc-client")
-//				.clientSecret("{noop}secret")
-//				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
-//				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-//				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
-//				.redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
-//				.postLogoutRedirectUri("http://127.0.0.1:8080/")
-//				.scope(OidcScopes.OPENID)
-//				.scope(OidcScopes.PROFILE)
-//				.clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
-//				.build();
-//
-//		return new InMemoryRegisteredClientRepository(oidcClient);
-//	}
-//
-//	@Bean 
-//	public JWKSource<SecurityContext> jwkSource() {
-//		KeyPair keyPair = generateRsaKey();
-//		RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
-//		RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
-//		RSAKey rsaKey = new RSAKey.Builder(publicKey)
-//				.privateKey(privateKey)
-//				.keyID(UUID.randomUUID().toString())
-//				.build();
-//		JWKSet jwkSet = new JWKSet(rsaKey);
-//		return new ImmutableJWKSet<>(jwkSet);
-//	}
-//
-//	private static KeyPair generateRsaKey() { 
-//		KeyPair keyPair;
-//		try {
-//			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-//			keyPairGenerator.initialize(2048);
-//			keyPair = keyPairGenerator.generateKeyPair();
-//		}
-//		catch (Exception ex) {
-//			throw new IllegalStateException(ex);
-//		}
-//		return keyPair;
-//	}
-//
-//	@Bean 
-//	public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
-//		return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
-//	}
-//
-//	@Bean 
-//	public AuthorizationServerSettings authorizationServerSettings() {
-//		return AuthorizationServerSettings.builder().build();
-//	}
+    @Bean 
+	public RegisteredClientRepository registeredClientRepository() {
+		RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+				.clientId("oidc-client")
+				.clientSecret("{noop}secret")
+				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+				.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+				.redirectUri("http://127.0.0.1:8080/login/oauth2/code/articles-client-oidc")
+				.postLogoutRedirectUri("http://127.0.0.1:8080/")
+				.scope(OidcScopes.OPENID)
+				.scope(OidcScopes.PROFILE)
+				.clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+				.build();
+
+		return new InMemoryRegisteredClientRepository(oidcClient);
+	}
+
+	@Bean 
+	public JWKSource<SecurityContext> jwkSource() {
+		KeyPair keyPair = generateRsaKey();
+		RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+		RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+		RSAKey rsaKey = new RSAKey.Builder(publicKey)
+				.privateKey(privateKey)
+				.keyID(UUID.randomUUID().toString())
+				.build();
+		JWKSet jwkSet = new JWKSet(rsaKey);
+		return new ImmutableJWKSet<>(jwkSet);
+	}
+
+	private static KeyPair generateRsaKey() { 
+		KeyPair keyPair;
+		try {
+			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+			keyPairGenerator.initialize(2048);
+			keyPair = keyPairGenerator.generateKeyPair();
+		}
+		catch (Exception ex) {
+			throw new IllegalStateException(ex);
+		}
+		return keyPair;
+	}
+
+	@Bean 
+	public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+		return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+	}
+
+	@Bean 
+	public AuthorizationServerSettings authorizationServerSettings() {
+		return AuthorizationServerSettings.builder().build();
+	}
 
 }
